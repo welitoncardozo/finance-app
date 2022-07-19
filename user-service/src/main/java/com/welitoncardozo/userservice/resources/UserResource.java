07b6b78620cea45a182280b9ebb49387ac88306e
Weliton Cardozo <welitoncardozo.br@gmail.com>
2022-07-18 21:40:47 -0300

    Change host to request.

--- user-service/src/main/java/com/welitoncardozo/userservice/resources/UserResource.java
+++ user-service/src/main/java/com/welitoncardozo/userservice/resources/UserResource.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("user")
+@RequestMapping("user-service")
 public class UserResource {
     private final UserService service;
 
